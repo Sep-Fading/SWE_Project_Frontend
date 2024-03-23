7b1f14dd9a36b5e7cc12151acacee9d1076d28f1
mathisweil <mathis.weil@outlook.com>
2024-03-23 00:27:02 +0000

    HomePage done

--- src/Components/HomeCard.tsx
+++ src/Components/HomeCard.tsx
@@ -1,27 +1,24 @@
-import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 interface HomeCardProps {
-    href: string;
-    image: string; 
-    text: string;
+  href: string;
+  image: string;
+  text: string;
+  layout: string;
 }
 
-const HomeCard: React.FC<HomeCardProps> = ({href, image, text}) => {
-
-    return (
-        <div className="flex items-center justify-center bg-fdm-gradient size-40 rounded-md drop-shadow-lg text-white">
-            <Link href={href}>
-                <div className="">
-                    <Image src={image} alt="Login" width={55} height={35} priority/>
-                </div>
-                <div>
-                    <h1>{text}</h1>
-                </div>
-            </Link>
-        </div>
-    );
+const HomeCard: React.FC<HomeCardProps> = ({ href, image, text, layout }) => {
+  return (
+    <Link
+      href={href}
+      className={`flex flex-col items-center justify-center gap-2 bg-fdm-gradient size-40 rounded-md drop-shadow-lg text-white ${layout}`}
+    >
+      <Image src={image} alt="Link" width={55} height={35} priority />
+      <h1>{text}</h1>
+    </Link>
+  );
 };
 
 export default HomeCard;
--- src/app/HomePage/page.tsx
+++ src/app/HomePage/page.tsx
@@ -1,11 +1,37 @@
 import Image from "next/image";
 import Navigation from "@/Components/Navigation";
+import HomeCard from "@/Components/HomeCard";
 
 export default function Home() {
   return (
     <div>
-      <Navigation />
-      <h1>Home Page</h1>
+      <Navigation links={[]} />
+      <div className="grid grid-cols-2 gap-6 p-10">
+        <HomeCard
+          href="/new_claim"
+          image="/new.svg"
+          text="New Claim"
+          layout="place-self-end"
+        />
+        <HomeCard
+          href="/edit_claim"
+          image="/edit.svg"
+          text="Edit Claim"
+          layout="place-self-start"
+        />
+        <HomeCard
+          href="/view_claims"
+          image="/view.svg"
+          text="View Claims"
+          layout="place-self-end"
+        />
+        <HomeCard
+          href="/request_help"
+          image="/request.svg"
+          text="Request Help"
+          layout="place-self-start"
+        />
+      </div>
     </div>
   );
 }
