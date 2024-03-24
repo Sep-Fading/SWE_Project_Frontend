1cd4b69d5c934ed2b2c02e634a26c854e6e5432d
Lewis Day <lewisday2901@outlook.com>
2024-03-24 20:37:05 +0000

    Added the Finance Pending Claims Page
    Added the finance pending claims and also refactored the ClaimInfoBox.tsx file so that the headings and buttons can be dynamically changed based on the page. Comments coming soon!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Navigation from "@/Components/Navigation";
 import HomeCard from "@/Components/HomeCard";
+import LineManagerPendingClaims from "./LM_pending_claims/page";
+import FinancePendingClaims from "./Finance_pending_claims/page";
 
 export default function Home() {
   const links = [
