63dd039eb1a38d25594eebc400ded9f7c7cb562b
mathisweil <mathis.weil@outlook.com>
2024-04-01 04:35:36 +0100

    Line manager homePage

--- src/app/Finance/page.tsx
+++ src/app/Finance/page.tsx
@@ -5,13 +5,13 @@ export default function Home() {
     <div className="grid grid-cols-2 gap-4 my-7">
       <HomeCard
         href="/Finance/pending_claims"
-        image="/edit.svg"
+        image="/approve.svg"
         text="Process Claims"
         layout="col-span-2 place-self-center w-[520px]"
       />
       <HomeCard
         href="/Finance/processed_claims"
-        image="/view.svg"
+        image="/list.svg"
         text="Processed Claims"
         layout="place-self-end row-start-2 w-[250px]"
       />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,13 @@ export default function Home() {
     <div className="grid grid-cols-2 gap-4 my-7">
       <HomeCard
         href="/new_claim"
-        image="/new.svg"
+        image="/add.svg"
         text="New Claim"
         layout="col-span-2 place-self-center w-[520px]"
       />
       <HomeCard
         href="/view_claims"
-        image="/view.svg"
+        image="/list.svg"
         text="View Claims"
         layout="place-self-end row-start-2 w-[250px]"
       />
