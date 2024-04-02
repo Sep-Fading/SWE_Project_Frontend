77976f31c7bbb5d12cb0d4d3e2aa967fdd4fab42
Lewis Day <lewisday2901@outlook.com>
2024-04-02 21:51:07 +0100

    Added grey background to claiminfobox so design is consistent

--- src/Components/ClaimInfoBox.tsx
+++ src/Components/ClaimInfoBox.tsx
@@ -9,7 +9,7 @@ interface ClaimInfoBoxProps {
 // The ClaimInfoBox element takes the headings and the text in the buttons as props as arrays - the data inside of these arrays are mapped into HTML elements (buttons and headings)
 const ClaimInfoBox = ({ headingText, buttonText }: ClaimInfoBoxProps) => {
   return (
-    <div className="flex">
+    <div className="flex shadow-lg rounded-md bg-[#D9D9D9]">
       <div className="flex flex-col justify-evenly">
         {headingText.map((heading, index) => (
           <h2 key={index}>{heading} :</h2>
